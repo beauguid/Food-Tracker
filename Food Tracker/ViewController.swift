216b891d350026a22787e40a2c9c706678cbbf7d
Guidry, Beau J <001332825@n102-a4.local>
2016-08-22 18:06:04 -0400

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Guidry, Beau J on 8/22/16.
+//  Copyright © 2016 Guidry, Beau J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
