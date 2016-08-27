39f0181a804b3e25180f2c76c39f70708e1bdb0e
Guidry, Beau J <001332825@n102-a4.local>
2016-08-26 20:39:37 -0400

    Added photo picker functionality.  Began creation of the new custom RatingControl

---
+++ Food Tracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by Guidry, Beau J on 8/26/16.
+//  Copyright © 2016 Guidry, Beau J. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,8 +21,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //Handle the text field's user input through delegate callbacks
         //this makes ViewController a delegate for nameTextField
         nameTextField.delegate = self
-        
-        
         
     }
 
@@ -44,7 +43,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        //dismiss the picker if the user cancelled
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //The info dictionary contains multiple representations of the image, and this uses the original
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //Set photoImageView to display the selected image
+        photoImageView.image = selectedImage
+        //dismiss the picker
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: AnyObject) {
+        //hide keyboard
+        nameTextField.resignFirstResponder()
+        
+        //creating the image picker controller variable instantiating a UIImagePickerController class
+        let imagePickerController = UIImagePickerController()
+        //selecting the sourcetype for the imagePickerController to the enumerated .PhotoLibrary type
+        imagePickerController.sourceType = .PhotoLibrary
+        //sets the delegate
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+        
+    }
     @IBAction func setDefaultLabelText(sender: UIButton) {
         nameTextField.text = "Default text"
         nameTextField.resignFirstResponder()
